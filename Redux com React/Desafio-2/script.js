d683459b3145d21474f424978e3c8651fa59e3cb
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-07-18 22:54:11 -0300

    Script modificado

--- Redux com React/Desafio-2/script.js
+++ Redux com React/Desafio-2/script.js
@@ -1,1 +1,14 @@
-/* script */
+import store from './store/configureStore.js';
+import { tokenFetch } from './store/token.js';
+import { userFetch } from './store/user.js';
+
+async function login(user) {
+  let state = store.getState();
+  if (state.token.data === null) {
+    await store.dispatch(tokenFetch(user));
+    state = store.getState();
+  }
+  await store.dispatch(userFetch(state.token.data));
+}
+
+login({ username: 'dog', password: 'dog' });
